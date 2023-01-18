09ef53121cf79ad2115d464ab67bd2fed2f12971
Galtonn <121112414+Galtonn@users.noreply.github.com>
2023-01-18 09:06:55 -0500

    level 2

--- Const.java
+++ Const.java
@@ -16,15 +16,14 @@ public final class Const{
     public static final int MOVE_SPEED = 8; // player move speed
     public static final int CHARW = 30;
     public static final int CHARH = 30;
-    public static final int STARTX = 50; // starting player X pos
-    public static final int STARTY = GROUND - CHARH - 40; // starting player Y pos
+    public static final int STARTX = 30; // starting player X pos
+    public static final int STARTY = GROUND - CHARH - 30; // starting player Y pos
     public static final int GRAVITY = (((CHARH+CHARW)/2)/20); // acceleration of gravity
     public static final int JUMP_SPEED = -(17*((CHARH+CHARW)/2)/30); // player jump speed
     public static final int STARTHP = 100;
     public static final int IFRAMES = 10;
     
-    public static final int TEXTSIZE = 30;
-    public static final Font TEXTFONT = new Font("Arial", Font.BOLD, TEXTSIZE);
+    public static final int EMOVESPEED = 5;
     
     public static final int CLIP_SIZE = 100;    
     public static final int BULLET_HEIGHT = 6;
@@ -34,8 +33,7 @@ public final class Const{
     public static final int SHOOTDELAY = 35; // how many frames before you can shoot again
     public static final int RELOADBARLENGTH = 50; // length of the yellow ammo refill bar
     
-    public static final int STAGETIME = 120; // 120 seconds to complete each level
-    public static final int STAGE1WIDTH = 8500;
+    public static final int STAGE1WIDTH = 6800;
     
     // new Platform(x, y, w, h, color);
     public static final Platform[] PLATFORMS1 = {
@@ -57,25 +55,26 @@ public final class Const{
         new Platform(6600,Const.GROUND,200,40,Color.GREEN),
         new Platform(7000,Const.GROUND,200,40,Color.GREEN),
         new Platform(7400,Const.GROUND,200,40,Color.GREEN),
-        new Platform(7800,Const.GROUND,800,40,Color.GREEN),
-//    new Platform(1000,Const.GROUND,600,40,Color.GREEN),
-//    new Platform(1800,Const.GROUND,200,40,Color.GREEN),
-//    new Platform(200, 400, 100, 50, Color.BLUE),
-//    new Platform(400, 200, 100, 50, Color.BLUE),
-//    new Platform(150, 100, 100, 50, Color.BLUE),
-//    new Platform(175, 310, 60, 20, Color.BLUE),
-//    new Platform(600, 380, 540, 30, Color.BLUE),
-//    new Platform(700, 230, 50, 50, Color.RED),
-//    new Platform(800, 230, 50, 50, Color.ORANGE),
-//    new Platform(900, 230, 50, 50, Color.YELLOW),
-//    new Platform(1000, 230, 83, 50, Color.GREEN),
-//    new Platform(1500, 200, 300, 40, Color.PINK),
-//    new Platform(300, 500, 40, 10, Color.CYAN),
-//    new Platform(390, 450, 40, 10, Color.CYAN),
-//    new Platform(480, 400, 80, 10, Color.CYAN),
-//    new Platform(850, 340, 40, 40, Color.MAGENTA),
-//    new Platform(890, 260, 10, 10, Color.MAGENTA),
-//    new Platform(1200, 300, 100, 20, Color.MAGENTA)
+        new Platform(7800,Const.GROUND,800,40,Color.GREEN)
+    };
+    public static final Platform[] PLATFORMS2 = {
+        new Platform(0,Const.GROUND,800,40,Color.GREEN),
+        new Platform(860,450,100,20,Color.GREEN),
+        new Platform(1100,500,100,20,Color.GREEN),
+        new Platform(1340,450,100,20,Color.GREEN),
+        new Platform(1580,500,100,20,Color.GREEN),
+        new Platform(1820,450,100,20,Color.GREEN),
+        new Platform(2060,450,700,20,Color.GREEN),
+        new Platform(2860,Const.GROUND,850,40,Color.GREEN),
+        new Platform(2830,450,830,20,Color.GREEN),
+        new Platform(3800,450,200,20,Color.GREEN),
+        new Platform(4100,Const.GROUND,900,40,Color.GREEN),
+        new Platform(4100,450,900,20,Color.GREEN),
+        new Platform(5200,500,100,20,Color.GREEN),
+        new Platform(5400,450,100,20,Color.GREEN),
+        new Platform(5600,500,100,20,Color.GREEN),
+        new Platform(5800,450,100,20,Color.GREEN),
+        new Platform(6000,Const.GROUND,800,40,Color.GREEN),
     };
     // new Enemy([x], [y], [w], [h], [speed], [direction], new Health([hp]), [damage]);
     public static final Enemy[] ENEMIES1 = {
@@ -89,10 +88,34 @@ public final class Const{
         new Enemy(6694, 400, 20, 20, 3, "left", new Health(10), 3),
         new Enemy(7086, 400, 20, 20, 3, "right", new Health(10), 3),
         new Enemy(7500, 400, 20, 20, 3, "left", new Health(10), 3),
-        new Enemy(8000, 400, 40, 80, 1, "left", new Health(20), 100),
-//    new Enemy(160, 60, 20, 40, 10, "left", new Health(20), 25),
-//    new Enemy(1600, 195, 5, 5, 6, "right", new Health(200), 34),
-//    new Enemy(300, -200, 100, 10, 5, "right", new Health(80), 10)
+        new Enemy(8000, 400, 40, 80, 1, "left", new Health(20), 100)
+    };
+    public static final Enemy[] ENEMIES2 = {
+        new Enemy(110, 0, 30, 30, 4, "left", new Health(5), 1),
+        new Enemy(198, 0, 30, 30, 4, "left", new Health(5), 1),
+        new Enemy(286, 0, 30, 30, 4, "left", new Health(5), 1),
+        new Enemy(390, 0, 30, 30, 4, "left", new Health(5), 1),
+        new Enemy(494, 0, 30, 30, 4, "left", new Health(5), 1),
+        new Enemy(582, 0, 30, 30, 4, "left", new Health(5), 1),
+        new Enemy(662, 0, 30, 30, 4, "left", new Health(5), 1),
+        new Enemy(2206, 350, 40, 100, 0, "left", new Health(100), 10),
+        new Enemy(2406, 350, 40, 100, 0, "left", new Health(100), 10),
+        new Enemy(2606, 350, 40, 100, 0, "left", new Health(100), 10),
+        new Enemy(3150, 100, 200, 200, 30, "right", new Health(400), 100),
+        new Enemy(3008, 520, 30, 30, 40, "left", new Health(10), 1),
+        new Enemy(3224, 520, 30, 30, 40, "left", new Health(10), 1),
+        new Enemy(3424, 520, 30, 30, 40, "left", new Health(10), 1),
+        new Enemy(4454, 100, 200, 200, 30, "right", new Health(400), 1),
+        new Enemy(4270, 520, 30, 30, 40, "left", new Health(10), 100),
+        new Enemy(4478, 520, 30, 30, 40, "left", new Health(10), 100),
+        new Enemy(4718, 520, 30, 30, 40, "left", new Health(10), 100),
+        new Enemy(6064, 0, 30, 30, 4, "left", new Health(5), 1),
+        new Enemy(6160, 0, 30, 30, 4, "left", new Health(5), 1),
+        new Enemy(6272, 0, 30, 30, 4, "left", new Health(5), 1),
+        new Enemy(6376, 0, 30, 30, 4, "left", new Health(5), 1),
+        new Enemy(6480, 0, 30, 30, 4, "left", new Health(5), 1),
+        new Enemy(6584, 0, 30, 30, 4, "left", new Health(5), 1),
+        new Enemy(6688, 0, 30, 30, 4, "left", new Health(5), 1),
     };
     
     private Const(){
